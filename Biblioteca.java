5dae775a879b5dd4e42551a64136178f372f4686
Mateo-mh <byronmh8@gmail.com>
2024-04-04 16:07:30 -0500

    Panel creado con exito

--- Biblioteca.java
+++ Biblioteca.java
@@ -19,5 +19,24 @@ public class Biblioteca extends JFrame implements ActionListener {
         setLayout(new BorderLayout());
         setResizable(false);
         setLocationRelativeTo(null);
+
+        //Panel formulario para agregar libros
+        JPanel formulario = new JPanel(new GridLayout(2, 2, 10, 10));
+        formulario.setBorder(BorderFactory.createEmptyBorder(20, 20, 10, 20));
+
+        JLabel lblTitulo = new JLabel("Titulo: ");
+        lblTitulo.setFont(new Font("Arial", Font.BOLD, 16));
+        txtTitulo = new JTextField();
+        txtTitulo.setFont(new Font("Arial", Font.PLAIN, 16));
+
+        JLabel lblAutor = new JLabel("Autor: ");
+        lblAutor.setFont(new Font("Arial", Font.BOLD, 16));
+        txtAutor = new JTextField();
+        txtAutor.setFont(new Font("Arial", Font.PLAIN, 16));
+
+        formulario.add(lblTitulo);
+        formulario.add(txtTitulo);
+        formulario.add(lblAutor);
+        formulario.add(txtAutor);
     }
 }
